f95b49280fabec7cf3ddaa1942060fdc4e331c38
mlisle <mlisle@protonmail.com>
2020-11-13 15:07:40 -0800

    Initial commit

---
+++ js/new-main.js
@@ -0,0 +1,49 @@
+/*----- app's state (variables) -----*/
+let runningTotal;
+
+
+/*----- cached element references -----*/
+let input1 = document.getElementById('num1');
+let buttonAdd = document.getElementById('add-button');
+let buttonSubtract = document.getElementById('subtract-button');
+let totalSpan = document.querySelector('span');
+
+
+/*----- event listeners -----*/
+buttonAdd.addEventListener('click', handleAdd);
+buttonSubtract.addEventListener('click', handleSubtract);
+
+
+/*----- functions -----*/
+function init() {
+    runningTotal = 0;
+    input1.value.innerText = `1`;
+    render();
+}
+
+function render() {
+    input1.value.innerText = `${input1}`;
+    if(runningTotal >= 0) {
+        totalSpan.innerHTML = `
+        <span> ${runningTotal} </span>
+        `
+    } else {
+        totalSpan.innerHTML = `
+        <span class="red"> ${runningTotal}</span>
+        `
+    }
+}
+
+function handleAdd() {
+    let num1 = parseInt(input1.value);
+    runningTotal += num1;
+    render();
+}
+
+function handleSubtract() {
+    let num1 = parseInt(input1.value);
+    runningTotal -= num1;
+    render();
+}
+
+init();
